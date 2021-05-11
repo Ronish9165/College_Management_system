9079e8d8c3b93deb2efc3f5bf3ae2a1c65fb02ce
Ronish <Ronish9165>
2021-05-11 15:09:45 +0545

    College Management System

--- college management system.py
+++ college management system.py
@@ -13,36 +13,35 @@ def information():
     top.geometry('1350x750')
     top.config(bg='sky blue')
 
-
-    Frame(top, width=50, height=500, bd=6).place(x=500, y=200)
     label = Label(top, text="Student Information", font=('arial', 20, "bold"), bg="sky blue", padx=10, pady=10)
     label.place(x=0, y=50, )
 
     name = Label(top, text="Name",font=('arial',15,'bold'), bg="sky blue")
-    name.place(x=20, y=180)
-    e1 = Entry(top, font=('arial',15,'bold'),).place(x=150, y=180, width=250, height=50)
+    name.place(x=20, y=190)
+    e1 = Entry(top, font=('arial',15,'bold'),).place(x=150, y=180, width=250, height=30)
 
     father_name = Label(top, text="Father Name",font=('arial',15,'bold'), bg="sky blue")
-    father_name.place(x=5, y=250)
-    e2 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=250, width=250, height=50)
+    father_name.place(x=5, y=260)
+    e2 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=250, width=250, height=30)
 
     mother_name = Label(top, text="Mother Name",font=('arial',15,'bold'), bg="sky blue")
     mother_name.place(x=2, y=310)
-    e3 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=310, width=250, height=50)
+    e3 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=310, width=250, height=30)
 
     address = Label(top, text="Address",font=('arial',15,'bold'), bg="sky blue")
     address.place(x=10, y=390)
-    e4 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=390, width=250, height=50)
+    e4 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=390, width=250, height=30)
 
     mobiile_number = Label(top, text="Mobile Number",font=('arial',15,'bold'), bg="sky blue")
     mobiile_number.place(x=0, y=450)
-    e5 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=450, width=250, height=50)
+    e5 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=450, width=250, height=30)
 
     dob = Label(top, text="Date of Birth",font=('arial',15,'bold'), bg="sky blue")
     dob.place(x=0, y=530)
-    e6 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=530, width=250, height=50)
+    e6 = Entry(top,font=('arial',15,'bold'),).place(x=150, y=530, width=250, height=30)
 
-    # gender = Label(top, text="Gender", bg="sky blue").place(x=0, y=310)
+    gender = Label(top, text="Gender",font=('arial',15,'bold'), bg="sky blue")
+    gender.place(x=0, y=610)
 
 
 def marksheet():
@@ -51,40 +50,54 @@ def marksheet():
     top.geometry('1350x750')
     top.config(bg='sky blue')
 
+    ern = Label(top, text="Enter Roll Number", font=('arial', 20, 'bold'), bg="sky blue")
+    ern.place(x=200, y=250)
+    e1 = Entry(top, font=('arial', 15, 'bold')).place(x=500, y=250, width=250, height=40)
+
+    sbutton= Button(top, text="SEARCH",font=('arial',15,'bold'), width=15, height=1,bg="lightgrey")
+    sbutton.place(x=200, y=330)
+
+    createbutton = Button(top, text="CREATE NEW ", font=('arial', 15, 'bold'), width=15, height=1, bg="lightgrey")
+    createbutton.place(x=500, y=330)
+
+    frame = LabelFrame(top, x=500, y=500, bg="black")
+    frame.place(x=100, y=100)
+
 def feereport():
     top = Toplevel()
     top.title('Fee Report')
     top.geometry('1350x750')
     top.config(bg='sky blue')
 
+    label = Label(top, text="Informations", font=('arial', 20, "bold"), bg="sky blue", padx=10, pady=10)
+    label.place(x=0, y=100)
 
     myLabel = Label(top, text= "Fee Report ", font=('arial',50, "bold"),bg="sky blue")
     myLabel.pack()
 
-    receipt = Label(top, text="Receipt No.", bg="sky blue")
-    receipt.place(x=10, y=50)
-    e1 = Entry(top).place(x=80, y=50, )
+    receipt = Label(top, text="Receipt No", font=('arial', 15, 'bold'), bg="sky blue")
+    receipt.place(x=20, y=190)
+    e1 = Entry(top, font=('arial', 15, 'bold'), ).place(x=150, y=180, width=250, height=30)
 
-    student_name = Label(top, text="Student Name", bg="sky blue")
-    student_name.place(x=0, y=90)
-    e2 = Entry(top).place(x=80, y=90, )
+    student_name = Label(top, text="Student Name", font=('arial', 15, 'bold'), bg="sky blue")
+    student_name.place(x=5, y=260)
+    e2 = Entry(top, font=('arial', 15, 'bold'), ).place(x=150, y=250, width=250, height=30)
 
-    admission = Label(top, text="Admission No.", bg="sky blue")
-    admission.place(x=2, y=130)
-    e3 = Entry(top).place(x=80, y=130)
+    admission = Label(top, text="Admission No", font=('arial', 15, 'bold'), bg="sky blue")
+    admission.place(x=2, y=310)
+    e3 = Entry(top, font=('arial', 15, 'bold'), ).place(x=150, y=310, width=250, height=30)
 
-    date = Label(top, text="Date", bg="sky blue")
-    date.place(x=10, y=170)
-    e4 = Entry(top).place(x=80, y=170)
+    date = Label(top, text="Date", font=('arial', 15, 'bold'), bg="sky blue")
+    date.place(x=10, y=390)
+    e4 = Entry(top, font=('arial', 15, 'bold'), ).place(x=150, y=390, width=250, height=30)
 
-    branch = Label(top, text="Branch", bg="sky blue")
-    branch.place(x=0, y=210)
-    e5 = Entry(top).place(x=80, y=210)
+    branch = Label(top, text="Branch", font=('arial', 15, 'bold'), bg="sky blue")
+    branch.place(x=0, y=450)
+    e5 = Entry(top, font=('arial', 15, 'bold'), ).place(x=150, y=450, width=250, height=30)
 
-    semester = Label(top, text="Semester", bg="sky blue")
-    semester.place(x=0, y=250)
-    e6 = Entry(top).place(x=80, y=250)
-
+    semester= Label(top, text="Semester", font=('arial', 15, 'bold'), bg="sky blue")
+    semester.place(x=0, y=530)
+    e6 = Entry(top, font=('arial', 15, 'bold'), ).place(x=150, y=530, width=250, height=30)
 
 
 
